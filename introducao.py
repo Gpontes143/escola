dc89e82d475d510244ce49d14c992f26a329afb4
Gpontes143 <Gpontes143@gmail.com>
2024-05-07 13:59:42 -0300

    first commit

---
+++ introducao.py
@@ -0,0 +1,11 @@
+n1 = 5
+n2 = 10 
+
+print(n1 + n2)
+
+nome = input('informe seu nome')
+idade = int(input('Informe sua idade'))
+alt = float(input('Informe sua altura '))
+
+
+print(f'Olá {nome}!, Você nasceu em {2024 - idade}')
