f5735ed532d16c6620372e76f07f2505ffb2f2ee
Gpontes143 <Gpontes143@gmail.com>
2024-05-07 14:48:35 -0300

    a

--- lista.py
+++ lista.py
@@ -1,32 +1,29 @@
 
 
 nome = list('SENAC')
-print(nome)
-
+#print(nome)
 numeros = list(range(1,16))
-
-print(numeros)
-
+#print(numeros)
 frutas = ['uva', 'banana', 'maça']
-print(frutas)
-
+#print(frutas)
 #adiciona um elemnto a lista
 frutas.append('morango')
-
-print(frutas)
-
+#print(frutas)
 #adiciona um elemnto a lista na posição indicada
 frutas.insert(1,'melão')
-
-print(frutas)
-
+#print(frutas)
 #extend addiciona itens para o final do item
 frutas.extend(['abacate', 'limão'])
-print(frutas)
+#print(frutas)
 #pop por padrão apaga o ultimo da lista
-
 frutas.pop(3)
+#print(frutas)
+#apaga um item da lista
 
+  
+notas = [5,8,7,4,2,6]
+print(notas)
+print(sorted(notas))
+print(sorted(notas, reverse = True))
+frutas.sort()
 print(frutas)
-
-frutas.remove(10)
